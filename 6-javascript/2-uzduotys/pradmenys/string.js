19b37040c5ffca8e57fa238357c513a8a0e3a31b
danielAleksiuk <d.aleksiuk@gmail.com>
2025-02-03 14:58:26 +0200

    add string part

--- 6-javascript/2-uzduotys/pradmenys/string.js
+++ 6-javascript/2-uzduotys/pradmenys/string.js
@@ -1,5 +1,27 @@
 console.log('================================');
 console.log('====   string uzduotys      ====');
 console.log('====   1 uzduotis:          ====');
+let eilute = 'labas';
+let newEilute = '';
+
+for(let i = eilute.length - 1; i >= 0; i--) {
+    newEilute += eilute[i];
+}
+
+console.log(eilute);
+console.log(newEilute);
+
 console.log('====   2 uzduotis:          ====');
+let sakinys = 'JAV analitikai: BTC kainuos 1 mln usd';
+let zodis = 'kainuos';
+
+console.log(sakinys);
+console.log(zodis);
+
+if (sakinys.includes(zodis)) {
+    console.log('yra');
+} else {
+    console.log('ne');
+}
+
 console.log('================================');
