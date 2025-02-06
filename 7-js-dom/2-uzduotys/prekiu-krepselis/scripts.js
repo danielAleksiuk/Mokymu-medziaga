b7ce94a4f6f396bb6f3d43f763b508dc6372d51d
danielAleksiuk <d.aleksiuk@gmail.com>
2025-02-06 21:47:19 +0200

    add dynamic content into prekiu krepselis task

---
+++ 7-js-dom/2-uzduotys/prekiu-krepselis/scripts.js
@@ -0,0 +1,76 @@
+function countFinalPrice(price, quantity) {
+    return price * quantity;
+}
+
+function countFinalOrderPrice(items) {
+    let sum = 0;
+    items.forEach(item => {
+        sum += item.finalPrice;
+    })
+
+    return sum;
+}
+
+function generateProductsHTML(products) {
+    let finalHtml = '';
+
+    products.forEach(product => {
+        finalHtml += `
+            <div class="row ${product.id}">
+                <div class="col-6"><p> ${product.name}</p></div>
+                <div class="col-2"><p> ${product.price} eur</p></div>
+                <div class="col-2">
+                    <p>
+                        <button class="remove-item">-</button>
+                        <span class="quantity">${product.quantity}</span>
+                        <button class="add-item">+</button>
+                    </p>
+                </div>
+                <div class="col-2">
+                    <p>
+                        <span class="final-price"> ${product.finalPrice}</span> eur
+                        <button class="delete-item">delete</button>
+                    </p>
+                </div>
+            </div>
+        `;
+    });
+
+    return finalHtml;
+}
+
+let products = [
+    {
+        id: 'sim-card',
+        name: 'Sim kortele',
+        price: 2.00,
+        quantity: 1,
+        imgUrl: 'img url'
+    },
+    {
+        id: 'phone',
+        name: 'Telefonas',
+        price: 1200.00,
+        quantity: 1,
+        imgUrl: 'img url'
+    },
+    {
+        id: 'sd-card',
+        name: 'SD kortele',
+        price: 35.00,
+        quantity: 2,
+        imgUrl: 'img url'
+    }
+];
+let finalOrderPrice = document.querySelector('.basket-price .price');
+
+products = products.map(product => {
+    return {
+        ...product,
+        finalPrice: countFinalPrice(product.price, product.quantity)
+    }
+});
+
+finalOrderPrice.innerHTML = countFinalOrderPrice(products).toFixed(2) + ' eur';
+
+document.querySelector('.product-list').innerHTML = generateProductsHTML(products);
