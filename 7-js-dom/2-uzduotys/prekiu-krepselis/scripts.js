7eecac1554dbd98aad94f23cff27a39ee58c51e9
danielAleksiuk <d.aleksiuk@gmail.com>
2025-02-10 19:33:53 +0200

    add delete action prekiu krepselis task

--- 7-js-dom/2-uzduotys/prekiu-krepselis/scripts.js
+++ 7-js-dom/2-uzduotys/prekiu-krepselis/scripts.js
@@ -11,30 +11,49 @@ function countFinalOrderPrice(items) {
     return sum;
 }
 
+function onDeleteButtonClick(id) {
+    // pasalinti produkta is product listo
+    products = products.filter(product => product.id !== id)
+    // 1 budas - istrinti div dali is html
+    
+    // 2 budas - atnaujinti krepseliu sarasa
+    productListHtml.innerHTML = generateProductsHTML(products);
+    finalOrderPrice.innerHTML = countFinalOrderPrice(products).toFixed(2) + ' eur';
+}
+
 function generateProductsHTML(products) {
     let finalHtml = '';
 
-    products.forEach(product => {
-        finalHtml += `
-            <div class="row ${product.id}">
-                <div class="col-6"><p> ${product.name}</p></div>
-                <div class="col-2"><p> ${product.price} eur</p></div>
-                <div class="col-2">
-                    <p class="quantity-section">
-                        <button class="remove-item">-</button>
-                        <span class="quantity">${product.quantity}</span>
-                        <button class="add-item">+</button>
-                    </p>
+    if (products.length) {
+        products.forEach(product => {
+            finalHtml += `
+                <div class="row ${product.id} product-item">
+                    <div class="col-6 product-name">
+                        <img src="${product.imgUrl}">
+                        <p> ${product.name}</p>
+                    </div>
+                    <div class="col-2"><p> ${product.price} eur</p></div>
+                    <div class="col-2">
+                        <p class="quantity-section">
+                            <button class="remove-item">-</button>
+                            <span class="quantity">${product.quantity}</span>
+                            <button class="add-item">+</button>
+                        </p>
+                    </div>
+                    <div class="col-2">
+                        <p class="final-price-section">
+                            <span class="final-price"> ${product.finalPrice} eur</span>
+                            <button class="delete-item" onClick="onDeleteButtonClick('${product.id}')">
+                                <i class="bi bi-trash"></i>
+                            </button>
+                        </p>
+                    </div>
                 </div>
-                <div class="col-2">
-                    <p class="final-price-section">
-                        <span class="final-price"> ${product.finalPrice} eur</span>
-                        <button class="delete-item"><i class="bi bi-trash"></i></button>
-                    </p>
-                </div>
-            </div>
-        `;
-    });
+            `;
+        });
+    } else {
+        finalHtml = '<div class="col-12"> Jusu krepselis yra tuscias</div>';
+    }
 
     return finalHtml;
 }
@@ -45,24 +64,25 @@ let products = [
         name: 'Sim kortele',
         price: 2.00,
         quantity: 1,
-        imgUrl: 'img url'
+        imgUrl: 'img/sim.png'
     },
     {
         id: 'phone',
         name: 'Telefonas',
         price: 1200.00,
         quantity: 1,
-        imgUrl: 'img url'
+        imgUrl: 'img/phone.png'
     },
     {
         id: 'sd-card',
         name: 'SD kortele',
         price: 35.00,
         quantity: 2,
-        imgUrl: 'img url'
+        imgUrl: 'img/sd.png'
     }
 ];
 let finalOrderPrice = document.querySelector('.basket-price .price');
+let productListHtml = document.querySelector('.product-list');
 
 products = products.map(product => {
     return {
@@ -72,5 +92,4 @@ products = products.map(product => {
 });
 
 finalOrderPrice.innerHTML = countFinalOrderPrice(products).toFixed(2) + ' eur';
-
-document.querySelector('.product-list').innerHTML = generateProductsHTML(products);
+productListHtml.innerHTML = generateProductsHTML(products);
