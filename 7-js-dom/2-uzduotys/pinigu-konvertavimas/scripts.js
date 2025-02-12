c50ae67f4b7b513ccd23b62d967ee7781b8b1d37
danielAleksiuk <d.aleksiuk@gmail.com>
2025-02-12 20:34:20 +0200

    add cureenct counting

--- 7-js-dom/2-uzduotys/pinigu-konvertavimas/scripts.js
+++ 7-js-dom/2-uzduotys/pinigu-konvertavimas/scripts.js
@@ -79,7 +79,16 @@ document.querySelector('.calculate').addEventListener('click', () => {
     let currencyFromValue = document.getElementById('input-from-value').value;
     let fromSelectValue = document.getElementById('select-currency-from').value;
     let toSelectValue = document.getElementById('select-currency-to').value;
-    console.log(fromSelectValue)
-    console.log(toSelectValue);
-    console.log(currencyFromValue)
+    // console.log(fromSelectValue)
+    // console.log(toSelectValue);
+    // console.log(currencyFromValue);
+
+    let currencyRate = currencies.find(currency => currency.id === fromSelectValue).rates.find(rate => rate.currencyId === toSelectValue).rate;
+        
+    let fromName = currencies.find(currency => currency.id === fromSelectValue).name;
+    let toName = currencies.find(currency => currency.id === toSelectValue).name;
+  
+    let changedCurrency = currencyFromValue * currencyRate;
+    console.log(`${currencyFromValue} ${fromName} yra ${changedCurrency} ${toName}`);
+    console.log(`santykis: 1 ${fromName} = ${currencyRate} ${toName}`)
 });
