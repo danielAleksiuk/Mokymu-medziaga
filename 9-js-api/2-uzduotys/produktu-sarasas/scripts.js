13e568c96243269cc6fa1e3e8e5f8785fed360f1
danielAleksiuk <d.aleksiuk@gmail.com>
2025-02-19 20:21:59 +0200

    add products task

--- 9-js-api/2-uzduotys/produktu-sarasas/scripts.js
+++ 9-js-api/2-uzduotys/produktu-sarasas/scripts.js
@@ -1,7 +1,19 @@
 function addProductsIntoDiv(products) {
-    console.log(products)
-}
+    let productsDiv = document.getElementById('products');
+    let productCard = '';
 
+    for (let product of products) {
+        productCard += `
+            <div class="product-card">
+                <img src="${product.image}">
+                <h4>${product.title}</h4>
+                <button class="button-card">perziureti</button>
+            </div>
+        `
+    }
+   
+    productsDiv.innerHTML = productCard;
+}
 
 
 window.addEventListener('load', () => {
