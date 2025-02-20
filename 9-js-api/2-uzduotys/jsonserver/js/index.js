4f3e68e44cc25ba1d2b4629f9c217605af35fc40
danielAleksiuk <d.aleksiuk@gmail.com>
2025-02-20 18:29:41 +0200

    add js-api: jsonserver task

---
+++ 9-js-api/2-uzduotys/jsonserver/js/index.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', () => renderPosts())
+
+const container = document.querySelector('.irasai')
+
+const renderPosts = async () => {
+    let uri = 'http://localhost:3000/posts'
+
+    const res = await fetch(uri)
+    const posts = await res.json()
+
+    let template = ''
+    posts.forEach(post => {
+            template += `
+            <div class="post">
+                <h2>${post.title}</h2>
+                <p><small>${post.likes} Likes</small></p>
+                <p>${post.body.slice(0, 200)}</p>
+                <a href="details.html?id=${post.id}">Plačiau</a>
+            </div>
+            `
+    });
+
+    container.innerHTML = template;
+};
