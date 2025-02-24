be7b0f9ee5ccd459201d920160c1fb5188122aed
danielAleksiuk <d.aleksiuk@gmail.com>
2025-02-24 21:25:47 +0200

    add indicators

--- 9-js-api/2-uzduotys/portfolio/website/projectsScript.js
+++ 9-js-api/2-uzduotys/portfolio/website/projectsScript.js
@@ -7,9 +7,15 @@ const getData = (url) => {
 const addProjectsIntoCarousel = (projects) => {
     let carouselHtml = document.querySelector('.carousel-inner');
 
-    carouselHtml.innerHTML = projects.map(project => {
+    carouselHtml.innerHTML = projects.map((project, index) => {
+        let activeClass = '';
+
+        if (index === 0) {
+            activeClass = 'active'
+        }
+
         return `
-            <div class="carousel-item">
+            <div class="carousel-item ${activeClass}">
                 <div class="carousel-caption d-none d-md-block">
                     <h5>${project.name}</h5>
                     <p>${project.description}</p>
@@ -21,12 +27,23 @@ const addProjectsIntoCarousel = (projects) => {
 
 const addIndicatorsIntoCarousel = (numberOfIndicators) => {
     let indicatorsHtml = document.querySelector('.carousel-indicators');
+
+    for (let i = 0; i< numberOfIndicators; i++ ) {
+        let activeClass = '';
+
+        if (i === 0) {
+            activeClass = 'class="active"'
+        }
+        indicatorsHtml.innerHTML += `
+            <button
+                type="button" ${activeClass} data-bs-target="#carouselExampleDark"
+                data-bs-slide-to="${i}" aria-current="true" aria-label="Slide ${i}">
+            </button>
+        `;
+    }
+
+    
 }
-<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        //   <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        //   <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-
-
 
 window.addEventListener('DOMContentLoaded', async () => {
     let projects = await getData('/projects');
