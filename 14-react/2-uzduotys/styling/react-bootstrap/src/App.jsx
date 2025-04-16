480250f558e9a025f6246ba13ef892793b364988
danielAleksiuk <d.aleksiuk@gmail.com>
2025-04-16 18:25:27 +0300

    boostrap react task

---
+++ 14-react/2-uzduotys/styling/react-bootstrap/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Card from './components/card/Card'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+
+function App() {
+
+  return (
+    <>
+        <Header/>
+        title 
+        description
+        <Card/> - 3 kartus (masyvas)
+        <Footer/>
+    </>
+  )
+}
+
+export default App
