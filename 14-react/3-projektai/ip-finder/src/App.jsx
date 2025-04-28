b760c6d85bb5505ca24d3da5119f7ff82a8a3487
danielAleksiuk <d.aleksiuk@gmail.com>
2025-04-28 19:05:29 +0300

    add map ip finder

--- 14-react/3-projektai/ip-finder/src/App.jsx
+++ 14-react/3-projektai/ip-finder/src/App.jsx
@@ -2,13 +2,11 @@
 
 import './App.css'
 
-import Map, {Marker}  from 'react-map-gl/mapbox';
-import 'mapbox-gl/dist/mapbox-gl.css';
 import Home from './pages/Home';
 
 function App() {
 
-  const API_KEY = 'pk.eyJ1IjoiZGFsZXg5MjEiLCJhIjoiY205djI1MWNpMGVxZTJwczY4b3FsY3UwaiJ9.ylfeJS2w4r5WfoXu9J5acQ';
+  
 
   return (
     <>
--- 14-react/3-projektai/ip-finder/src/pages/Home.jsx
+++ 14-react/3-projektai/ip-finder/src/pages/Home.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import { GiAbstract005 } from "react-icons/gi";
+import { MAP_API_KEY } from "../utils/constants";
 
+import './Home.css';
+
+import Map, {Marker}  from 'react-map-gl/mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css';
 // latitude: 54.6876
 // longitude: 25.2806
 
 const Home = () => {
     const [ipDetails, setIpDetails] = useState();
+    const [coordinates, setCoordinates] = useState({
+      latitude: 55.261909,
+      longitude: 24.034855
+    });
+    const [viewPort, setViewPort] = useState({
+      latitude: coordinates.latitude,
+      longitude: coordinates.longitude,
+      zoom: 5,
+      width: '100%',
+      height: '100%'
+    });
+
+    // const [lat, setLat] = useState();
+    // const [lon, setLon] = useState();
     
   useEffect(() => {
     fetch('https://ipapi.co/json/')
@@ -14,8 +32,20 @@ const Home = () => {
       .then(data => {
         console.log(data);
         setIpDetails(data);
+        setCoordinates({
+          latitude: data.latitude,
+          longitude: data.longitude
+        });
+
+        // setLat(data.latitude);
+        // setLon(data.longitude)
       })
   }, []);
+
+  useEffect(() => {
+    console.log('coordinates is here')
+    console.log(coordinates)
+  }, [coordinates])
 
     return (
         <div>
@@ -26,6 +56,15 @@ const Home = () => {
             <p>{ipDetails && ipDetails.country_name},{ipDetails?.region} </p>
             <p>internet provider</p>
             <p>{ipDetails?.org}</p>
+
+            <div className="mapContainer">
+                <Map
+                  mapboxAccessToken={MAP_API_KEY}
+                  {...viewPort}
+                  mapStyle="mapbox://styles/mapbox/streets-v9">
+
+                </Map>
+            </div>
         </div>
     )
 };
