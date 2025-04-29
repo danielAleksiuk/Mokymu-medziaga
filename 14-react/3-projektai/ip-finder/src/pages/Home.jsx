679e433d975358531c22d5bec77003be57110841
danielAleksiuk <d.aleksiuk@gmail.com>
2025-04-29 22:51:58 +0300

    add use fetch hooks and loader

--- 14-react/3-projektai/ip-finder/src/pages/Home.jsx
+++ 14-react/3-projektai/ip-finder/src/pages/Home.jsx
@@ -3,93 +3,94 @@ import { useEffect } from "react";
 import './Home.css';
 import MapContainer from "../components/MapContainer";
 import LabelWithTitle from "../components/LabelWithTitle";
+import useFetch from "../hooks/useFetch";
 
 
 // latitude: 54.6876
 // longitude: 25.2806
 
 const Home = () => {
-    const [ipDetails, setIpDetails] = useState({
-      ip: null,
-      city: null,
-      org: null,
-      country_name: null
-    });
-    const [coordinates, setCoordinates] = useState({
-      latitude: 55.261909,
-      longitude: 24.034855
-    });
-    
-    // useFetch
-    // api path
+  const [ipDetails, setIpDetails] = useState({
+    ip: null,
+    city: null,
+    org: null,
+    country_name: null
+  });
+  const [coordinates, setCoordinates] = useState({
+    latitude: 55.261909,
+    longitude: 24.034855
+  });
+  const {data, loading, makeApiCall} = useFetch('https://ipapi.co/json/');
 
   useEffect(() => {
-    fetch('https://ipapi.co/json/')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setIpDetails({
-          ip: data.ip,
-          city: data.region,
-          org: data.org,
-          country_name: data.country_name
-        });
-        setCoordinates({
-          latitude: data.latitude,
-          longitude: data.longitude
-        });
-      })
-      .catch(() => {
-        // fake data 
-        setIpDetails({
-          ip: '78.60.184.222',
-          city: 'Random city',
-          org: 'TEO',
-          country_name: 'Random Country'
-        });
-        setCoordinates({
-          latitude: 19.932344,
-          longitude: 78.254511
-        });
-      })
+      const getIpData = async () => {
+        await makeApiCall();  
+      }
+      
+      getIpData();
   }, []);
 
- 
+  useEffect(() => {
+    if (data) {
+      const {ip, city, org, country_name, latitude, longitude } = data;
+      setCoordinates({latitude, longitude});
+      setIpDetails({ip, city, org, country_name});
+    }
+  }, [data])
 
     return (
       <>
-        <LabelWithTitle 
-          title='IP address finder'
-          type="primary"
-          align="center"
-        />
+        { 
+          loading && (
+            <>
+              <LabelWithTitle 
+                title='Data is loading'
+                type="primary"
+                align="center"
+              />
+              <div className="loader"></div>
+            </>
+          )
+        }
+        {
+          !loading && (
+            <>
+               <LabelWithTitle 
+                  title='IP address finder'
+                  type="primary"
+                  align="center"
+                />
 
-        <LabelWithTitle 
-          title='What is my IP address?'
-          type="secondary"
-          body={ipDetails.ip}
-          align="left"
-        />
-        
-        <LabelWithTitle
-          title='Location'
-          type="default"
-          body={ `${ipDetails.country_name}, ${ipDetails.city}` }
-          align="left"
-        />
+                <LabelWithTitle 
+                  title='What is my IP address?'
+                  type="secondary"
+                  body={ipDetails.ip}
+                  align="left"
+                />
+                
+                <LabelWithTitle
+                  title='Location'
+                  type="default"
+                  body={ `${ipDetails.country_name}, ${ipDetails.city}` }
+                  align="left"
+                />
 
-        <LabelWithTitle 
-          title='Internet provider'
-          type="default"
-          body={ipDetails.org}
-          align="right"
-        />
+                <LabelWithTitle 
+                  title='Internet provider'
+                  type="default"
+                  body={ipDetails.org}
+                  align="right"
+                />
 
-        <div className="mapContainer">
-          <MapContainer
-            latitude={coordinates.latitude}
-            longitude={coordinates.longitude}/>
-        </div>
+                <div className="mapContainer">
+                  <MapContainer
+                    latitude={coordinates.latitude}
+                    longitude={coordinates.longitude}/>
+                </div>
+            </>
+          )
+        }
+       
       </>
     )
 };
