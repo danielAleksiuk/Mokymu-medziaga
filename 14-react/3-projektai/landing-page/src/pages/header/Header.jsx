7ea1a8bd5ff1998b43bcb67cef949b74ebcb9d01
danielAleksiuk <d.aleksiuk@gmail.com>
2025-05-05 18:22:52 +0300

    add item details

--- 14-react/3-projektai/landing-page/src/pages/header/Header.jsx
+++ 14-react/3-projektai/landing-page/src/pages/header/Header.jsx
@@ -4,21 +4,16 @@ import './Header.scss';
 const Header = (props) => {
     return (
         <div className="header">
-            <div  className="header__overlay">
-            <Container>
-                <Row className="header__details">
-                    <h1 className="header__title">{ props.title }</h1>
-                    <p className="header__text">{props.subtitle}</p>
-                    <Button className="header__button">Learn more</Button>
-                </Row>
-            </Container>
+            <div className="header__overlay">
+                <Container>
+                    <Row className="header__details">
+                        <h1 className="header__title">{ props.title }</h1>
+                        <p className="header__text">{props.subtitle}</p>
+                        <Button className="header__button">{props.button}</Button>
+                    </Row>
+                </Container>
             </div>
-            
-            
         </div>
-        
-
-        // ['iconName']()
     )
 }
 
