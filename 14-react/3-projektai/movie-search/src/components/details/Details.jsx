9ceb6059c0d63643387f06064327e431fe58ba18
danielAleksiuk <d.aleksiuk@gmail.com>
2025-05-14 19:14:51 +0300

    add fetch api part

--- 14-react/3-projektai/movie-search/src/components/details/Details.jsx
+++ 14-react/3-projektai/movie-search/src/components/details/Details.jsx
@@ -1,8 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const API_URL = 'https://www.omdbapi.com'
+const API_KEY = 'apikey=a2526df0';
+
 
 const Details = ({id, handleClose}) => {
+    const [movie, setMovie] = useState();
+
+
     useEffect(() => {
-        console.log(id);
+        const getData = () => {
+            const url = `${API_URL}/?${API_KEY}&i=${id}`;
+
+            fetch(url)
+                .then(response => response.json())
+                .then(data => console.log(data));
+        }
+        
+        getData();
+
+
     }, [id])
 
     return (
