2f913f1459c77a0a19ee5ff5514f69b9eb3a5583
danielAleksiuk <d.aleksiuk@gmail.com>
2025-05-19 19:25:04 +0300

    add animation

--- 14-react/3-projektai/wordle/client/src/components/Row.jsx
+++ 14-react/3-projektai/wordle/client/src/components/Row.jsx
@@ -1,6 +1,24 @@
-const Row = ({guess}) => {
+const Row = ({guess, currentGuess}) => {
     return (
         <>
+            {
+                currentGuess && (
+                    <div className="row current">
+                        {
+                            currentGuess.split('').map((letter, index) => (
+                                <div key={index} className="filled">
+                                    {letter}
+                                </div>
+                            ))
+                        }
+                        {
+                            [...Array(5 - currentGuess.split('').length)].map((_, index) => (
+                                <div key={index}></div>
+                            ))
+                        }
+                    </div>
+                )
+            }
             {
                 guess && (
                     <div className="row past">
