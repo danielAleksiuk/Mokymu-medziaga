c1b5d03a31bd012ec62e49944c308f43600d4536
danielAleksiuk <d.aleksiuk@gmail.com>
2025-06-18 18:09:06 +0300

    add something

--- 16-back-end+databases/1-paskaitos/expressjs-intro/app.js
+++ 16-back-end+databases/1-paskaitos/expressjs-intro/app.js
@@ -2,6 +2,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const mongoose = require('mongoose');
+
+const dbUrl = 'mongodb+srv://codingSchoolDaniel:GKLomYzKfAuQ9SUj@coding-school-cluster.ehftrvr.mongodb.net/sample_mflix';
+
+mongoose.connect(dbUrl)
+    .then(result => {
+        console.log('prie DB pasijungiau');
+        console.log(result)
+    })
+    .catch(error => console.log(error));
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    // _id: {type: Schema.Types.ObjectId },
+    name: {type: String},
+    email: {type: String},
+    password: {type: String}
+}, {collection: 'users'})
+
+
+
+// sample_mflix.users
+const User = mongoose.model('user', userSchema);
+// const collectionName = User.collection.collectionName;
 
 app.set('view engine', 'ejs');
 
@@ -9,7 +34,7 @@ app.set('view engine', 'ejs');
 //     res.send(
 //         '<h1>hello mano pirmoji express uzklausa</h1>'
 //     );
-// });
+// });v
 
 
 // app.listen(port)
@@ -40,11 +65,24 @@ app.get('/', (req, res) => {
         {title: 'mongo db pamoka', santrauka: 'lorem lorem'}
     ]
 
+    // console.log(collectionName);
+    // console.log(User)
+
+    User.find().limit(10)
+        .then(result =>console.log(result))
+        .catch(error => console.log(error));
+
     res.render('./ejs/index', {blogs});
 });
 
 
 app.get('/apie', (req, res) => {
+    new User({
+        name: 'mano-pvz',
+        email: 'pvz-email',
+        password: 'pvz0pvz'
+    }).save().then(result => console.log(result));
+
     res.render('./ejs/apie', {manoInfo: 'mano info is serviso'});
 });
 
@@ -54,4 +92,20 @@ app.get('/createBlog', (req, res) => {
 
 app.post('/create-blog', (req, res) => {
     console.log(req);
+});
+
+app.get('/userList', async (req, res) => {
+    const useriai = await User.find().limit(25);
+    console.log(useriai);
+    res.render('./ejs/userList', {users: useriai});
 })
+
+//  user - codingSchoolDaniel
+//  psw - GKLomYzKfAuQ9SUj
+
+
+
+// 1. papildyti navbar - user list
+// 2. sukurti user listo puslapi
+// 3. atvaizduoti is DB gautus userius
+// 4. html pavidalu su lentele
