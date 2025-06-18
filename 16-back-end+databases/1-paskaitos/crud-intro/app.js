2643446a37bb091e46896c2881222a89730d2d98
danielAleksiuk <d.aleksiuk@gmail.com>
2025-06-18 19:06:24 +0300

    crud intro

---
+++ 16-back-end+databases/1-paskaitos/crud-intro/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = 3000;
+const dbUrl = 'mongodb+srv://codingSchoolDaniel:GKLomYzKfAuQ9SUj@coding-school-cluster.ehftrvr.mongodb.net/crud_example';
+
+mongoose.connect(dbUrl)
+    .then(() => app.listen(PORT))
+    .catch(error => console.log(error));
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname));
+app.use(express.urlencoded({extended: true}));
+
+
---
+++ 16-back-end+databases/1-paskaitos/crud-intro/models/receptas.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ReceptasSchema = new Schema({
+    title: {type: String, required: true},
+    level: {type: Number, required: true}, // reikia ideti type, toki pat kaip mongoDB
+    description: {type: String, required: true},
+    price: {type: Number, required: true}
+}, {timestamps: true});
+
+const Receptas = mongoose.model('Receptas', ReceptasSchema);
+
+module.exports = Receptas;
+
