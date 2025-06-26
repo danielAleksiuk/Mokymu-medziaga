ef2001b4861c015f0ec1d827c78b46f16beb1c6d
danielAleksiuk <d.aleksiuk@gmail.com>
2025-06-26 18:41:37 +0300

    node auth init

---
+++ 16-back-end+databases/1-paskaitos/node-auth/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const dbUrl = 'mongodb+srv://codingSchoolDaniel:GKLomYzKfAuQ9SUj@coding-school-cluster.ehftrvr.mongodb.net/node-auth';
+
+mongoose.connect(dbUrl)
+    .then(() => {
+        app.listen(3000);
+        console.log('app launched;database connected');
+    })
+    .catch(error => console.log(error));
+
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => res.render('home'));
